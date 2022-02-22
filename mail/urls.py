2158a64ba220641231be4fdd250045468e80b4f4
Vityarock <bvs@maxidom.ru>
2022-02-22 19:16:39 +0300

    new repo update1

--- mail/urls.py
+++ mail/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from sendmail.views import EmailView
+from sendmail.views import EmailView, EmailCreate
 
 urlpatterns = [
     path('admin/', admin.site.urls),
